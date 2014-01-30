bd7095074eee0ce210769b5e5c88c4b9b8b3c763
Ikradex <ikradex@gmail.com>
2014-01-30 21:55:26 +0000

    Init app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CharitySystemManager::Application.config.secret_key_base = '079e0ee93c9e04272ab5967b8c68e55687285837528ce8f26c80950d574f1dca22db01f22e31d33676d036d32eb96bf18e1bfeadb8fbcc998303d522c496e86f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CharitySystemManager::Application.config.session_store :cookie_store, key: '_charity_system_manager_session'
